d8779f21a0931246aa3d79d79f8775d295f06fad
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-18 19:51:15 +0000

    test

--- mysite/program/urls.py
+++ mysite/program/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.views import generic
 
 urlpatterns = [
-    path('', generic.TemplateView.as_view(template_name='index.html'))
+    path('', views.MyView.as_view(result='fffasdkgjasdfgkl;vrn'))
 ]
--- mysite/program/views.py
+++ mysite/program/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class MyView(View):
+    result='hi'
+    def get(self, request):
+
+        return HttpResponse(self.result)
+
+class greet(MyView):
+    result='bye'
