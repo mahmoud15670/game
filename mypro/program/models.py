0a13773f1aebbc6bacb488809eaaee4a296e233b
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-22 12:35:26 +0000

    good

--- mypro/program/models.py
+++ mypro/program/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -5,14 +6,34 @@ class Patient(models.Model):
     name = models.CharField(max_length=40)
     age = models.PositiveSmallIntegerField()
     date = models.CharField(max_length=5, choices=[
-        ('year','year')
-        ('month','month')
+        ('year','year'),
+        ('month','month'),
         ('day','day')
     ])
     gender = models.CharField(max_length=6, choices=[
         ('male','male'),
         ('female','female')
     ])
-    dr = models.CharField(max_length=40)
-    phone = models.CharField(max_length=11)
-    
+    dr = models.CharField(max_length=40, blank=True)
+    phone = models.CharField(max_length=11, blank=True)
+    tests = models.ManyToManyField('Test')
+    entry = models.DateTimeField(default=timezone.now(), editable=False)
+
+    def __str__(self):
+        return self.name
+
+class Test(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+    unit = models.CharField(max_length=20, blank=True)
+    male_ref = models.TextField(blank=True)
+    female_ref = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
+    test = models.ForeignKey('Test', on_delete=models.CASCADE)
+    result = models.CharField(max_length=20)
+    ref = models.TextField()
--- mypro/program/urls.py
+++ mypro/program/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-
+    path('', views.IndexView.as_view(), name='index'),
+    path('patient', views.PatientCreateView.as_view(), name='create_patient'),
+    path('patient/<int:pk>', views.PatientDetilView.as_view(), name='patient_detil'),
+    path('patient/edit/<int:pk>', views.PatientEditView.as_view(), name='patient_edit'),
+    path('patient/delete/<int:pk>', views.PatientDeleteView.as_view(), name='patient_delete'),
+    path('test', views.TestCreateView.as_view(), name='create_test'),
+    path('test/<int:pk>', views.TestDetilView.as_view(), name='test_detil'),
+    path('test/edit/<int:pk>', views.TestEditView.as_view(), name='test_edit'),
+    path('test/delete/<int:pk>', views.TestDeleteView.as_view(), name='test_delete'),
 ]
